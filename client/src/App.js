09172ea2033ea9779b7552b045c860760d2545ba
Daniil Khlyvniuk <daniil.khlyv@gmail.com>
2021-10-28 20:39:50 +0300

    added ESLint

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import './App.css'
+import React from 'react'
 
 function App() {
-  return (
-    <div className="App">
-			<h1>F I N A L     P R O J E C T</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>F I N A L P R O J E C T</h1>
+		</div>
+	)
 }
 
-export default App;
+export default App
