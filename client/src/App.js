52206c965f91131878677fd82895c78921d4560d
Vitaliy Gukalov <gukalov.v@ukr.net>
2021-10-29 20:54:33 +0300

    Add Header component part1

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import NavBar from './components/NavBar/NavBar'
+
 
 const App = () => {
 	return (
 		<div className={ 'App' }>
+			<NavBar />
 			<h1>F I N A L P R O J E C T</h1>
 		</div>
 	)
---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+
+const Navbar = () => {
+	return (
+		<header>
+			<AppBar>
+				<Toolbar>
+                    Header
+				</Toolbar>
+			</AppBar>
+		</header>
+	)
+}
+
+export default Navbar
