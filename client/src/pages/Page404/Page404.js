7925281f56f0f8ad57b7691187a1a149a83b0155
Bobshop verbytski <nikolasdelunko@gmail.com>
2021-10-30 23:40:20 +0300

    404 Done

--- client/src/pages/Page404/Page404.js
+++ client/src/pages/Page404/Page404.js
@@ -2,11 +2,22 @@ import React from 'react'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 import './Page404.scss'
+
+
+const theme = createTheme({
+	palette: {
+		neutral: {
+			main: '#373f41',
+			contrastText: '#fff',
+		},
+	},
+})
 
 const Error404 = () => {
 	return (
-		<Grid container direction={'column'} alignItems='center' className={ 'errorPage' } spacing={{ md: 10 }}>
+		<Grid container direction={'column'} alignItems='center' className={ 'errorPage' } spacing={{ xs: 9}}>
 			<Grid item>
 				<Typography variant={'h1'}>
         404
@@ -23,7 +34,9 @@ const Error404 = () => {
 				</Typography>
 			</Grid>
 			<Grid item>
-				<Button className={ 'submit' } variant="contained">Home Page</Button>
+				<ThemeProvider theme={theme}>
+					<Button color="neutral" variant="contained">Home Page</Button>
+				</ThemeProvider>
 			</Grid>
 		</Grid>
 	)
