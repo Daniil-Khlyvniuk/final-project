e58379b08e7fd15bfe764d655329dd28f6b464e9
Elizabeth Rudaya <lizarudayaa@gmail.com>
2021-10-30 03:37:16 +0300

    Added Error Boundary component and page 500

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary'
 
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<ErrorBoundary>
+			<App />
+		</ErrorBoundary>
 	</React.StrictMode>,
 	document.getElementById('root')
 )
