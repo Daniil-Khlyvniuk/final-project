b6a85fb4ae58a12bfcdaf9f05c03dfc86319c461
Elizabeth <77597107+lizarudayaa@users.noreply.github.com>
2021-10-30 18:59:14 +0300

    Added loader (#5)

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import NavBar from './components/NavBar/NavBar'
 import { Typography } from '@mui/material'
 
+
 const App = () => {
 	return (
 		<div className={ 'App' }>
