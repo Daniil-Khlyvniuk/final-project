ad68d0ac8aa3ff966c06331d3745ea0083530d11
Aleksandr Mayboroda <aleksandr.mayboroda@gmail.com>
2021-10-30 12:43:52 +0300

    before develop merge commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import Footer from './components/Footer'
+
 const App = () => {
 	return (
 		<div className={ 'App' }>
 			<h1>F I N A L P R O J E C T</h1>
+			<Footer />
 		</div>
 	)
 }
