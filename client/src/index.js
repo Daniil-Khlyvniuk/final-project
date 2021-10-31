4919cec766bc7eb790a7d967b757424eb695d017
IggyDev34 <82322541+IggyDev34@users.noreply.github.com>
2021-10-31 08:48:33 +0200

    Routes (#9)
    * created routes and the ProtectedRoute component
    
    * added login route
    
    * fixed 404 path
    
    Co-authored-by: Igor Nikonov <igor.nikonov.07@gmail.com>

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from '@mui/material/styles'
 import Theme from './utils/Theme'
 
+
 ReactDOM.render(
 	<ThemeProvider theme={Theme}>
 		<React.StrictMode>
