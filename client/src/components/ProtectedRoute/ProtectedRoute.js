09a3b75e2697fae1a2fd6f0e5d3b6d81b24b8109
IggyDev34 <iggydev34@gmail.com>
2021-10-31 14:23:53 +0200

    eslint

--- client/src/components/ProtectedRoute/ProtectedRoute.js
+++ client/src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
 
-
 const ProtectedRoute = ({isLoggedIn, children, ...rest}) => {
-  return (
-    <Route {...rest}>
-      {isLoggedIn ? children : <Redirect to='/login' />}
-    </Route>
-  )
+	return (
+		<Route {...rest}>
+			{isLoggedIn ? children : <Redirect to='/login' />}
+		</Route>
+	)
 }
 
 export default ProtectedRoute
--- client/src/staticPages/PaymentAndDelivery/PaymentAndDelivery.js
+++ client/src/staticPages/PaymentAndDelivery/PaymentAndDelivery.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const PaymentAndDelivery = () => {
-  return (
-    <h1>
-      PATMENT AND DELIVERY
-    </h1>
-  )
+	return (
+		<div>
+			<h1>PaymentAndDelivery</h1>
+		</div>
+	)
 }
 
 export default PaymentAndDelivery
