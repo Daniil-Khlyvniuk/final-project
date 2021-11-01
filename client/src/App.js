e6d6e450b8e1fd7c7d92079c2d95eb68d915f95d
Bobshop verbytski <nikolasdelunko@gmail.com>
2021-11-01 10:53:51 +0200

    Del scss and node sass

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Error404 from './pages/Page404/Page404'
+
 
 function App() {
 	return (
 		<div className={ 'App' }>
 			<h1>F I N A L P R O J E C T</h1>
+			<Error404 />
 		</div>
 	)
 }
--- client/src/pages/Page404/Page404.js
+++ client/src/pages/Page404/Page404.js
@@ -2,29 +2,20 @@ import React from 'react'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
-import './Page404.scss'
+import {ThemeProvider} from '@mui/material/styles'
+import theme from '../../utils/Theme'
 
-
-const theme = createTheme({
-	palette: {
-		neutral: {
-			main: '#373f41',
-			contrastText: '#fff',
-		},
-	},
-})
 
 const Error404 = () => {
 	return (
-		<Grid container direction={'column'} alignItems='center' className={ 'errorPage' } spacing={{ xs: 9}}>
+		<Grid style={{backgroundImage: 'url(https://telegra.ph/file/3cc57f6e2278684805a33.jpg)', backgroundSize: 'cover'}} container direction={'column'} alignItems='center' className={ 'errorPage' } spacing={{ xs: 9}}>
 			<Grid item>
 				<Typography variant={'h1'}>
         404
 				</Typography>
 			</Grid>
 			<Grid item>
-				<Typography variant={'h2'}>
+				<Typography  variant={'h2'}>
 					Oops. Looks like you came to wrong page on our server
 				</Typography>
 			</Grid>
@@ -35,7 +26,7 @@ const Error404 = () => {
 			</Grid>
 			<Grid item>
 				<ThemeProvider theme={theme}>
-					<Button color="neutral" variant="contained">Home Page</Button>
+					<Button color="primary" variant="contained">Home Page</Button>
 				</ThemeProvider>
 			</Grid>
 		</Grid>
