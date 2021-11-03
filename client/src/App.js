f95eb08a63f8b5e3e53f675531d66a48de53617c
IggyDev34 <82322541+IggyDev34@users.noreply.github.com>
2021-11-03 12:45:53 +0200

    Redux toolkit (#30)
    * package-lock
    
    * initial redux configuration created

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,17 @@ import Header from './components/NavBar/NavBar'
 import AppRoutes from './routes/AppRoutes'
 import Footer from './components/Footer'
 import { Container } from '@mui/material'
+import cardActions from './store/Card'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+	// it is an example of dispatching actions
+	const dispatch = useDispatch()
+	dispatch(cardActions.test())
+	dispatch(cardActions.test2('it works'))
+
+	//========================================
+
 	return (
 		<div className={'App'}>
 			<Header />
