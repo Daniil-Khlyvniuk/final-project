63c9aa0ccfffd7e35f6d97e3e387ea0dfa4c9d52
Daniil Khlyvniuk <daniil.khlyv@gmail.com>
2021-11-04 10:07:30 +0200

    returned container to navBar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AppBar, Toolbar } from '@mui/material'
+import { AppBar, Container, Toolbar } from '@mui/material'
 import NavBarLogo from './NavBarLogo/NavBarLogo'
 import { Box } from '@mui/system'
 import LoginIcon from './NavBarIcons/LoginIcon/LoginIcon'
@@ -13,22 +13,24 @@ const Navbar = () => {
 	const classes = useStyles()
 
 	return (
-		<Box sx={{ borderColor: '#373F41', borderBottom: 1 }}>
-			<AppBar position="static" sx={{ boxShadow: 'none' }}>
-				<Toolbar className={ classes.header }>
-					<NavBarLogo/>
-					{/*<NavbarMenu />*/ }
-					<Category/>
-					{/* <Navbarlist /> */ }
-					<NavBarSearch/>
-					<div className={ classes.iconsWrapper }>
-						<LoginIcon/>
-						<FavoriteIcon/>
-						<CartIcon/>
-					</div>
-					{/* <NavBarLanguages /> */ }
-				</Toolbar>
-			</AppBar>
+		<Box sx={ { borderColor: '#373F41', borderBottom: 1 } }>
+			<Container maxWidth="lg">
+				<AppBar position="static" sx={ { boxShadow: 'none' } }>
+					<Toolbar className={ classes.header }>
+						<NavBarLogo/>
+						{/*<NavbarMenu />*/ }
+						<Category/>
+						{/* <Navbarlist /> */ }
+						<NavBarSearch/>
+						<div className={ classes.iconsWrapper }>
+							<LoginIcon/>
+							<FavoriteIcon/>
+							<CartIcon/>
+						</div>
+						{/* <NavBarLanguages /> */ }
+					</Toolbar>
+				</AppBar>
+			</Container>
 		</Box>
 	)
 }
