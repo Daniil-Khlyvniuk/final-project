7e9541ab05d0a5463ef142b0c1396a53013f3fdd
otroshenkodarina <otroshenkodarina@gmail.com>
2021-11-04 21:16:11 +0200

    fixed

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,6 @@ const App = () => {
 
 	return (
 		<div className={ 'App' }>
-			<h1>F I N A L P R O J E C T</h1>
 			<Container maxWidth="lg">
 				<AppRoutes/>
 			</Container>
@@ -27,12 +26,6 @@ const App = () => {
 			<Catalog />
 			<NavBar />
 			<Footer />
-			<Typography
-				variant={'h2'}
-				sx={{color: 'primary', fontSize:'primary'}}
-			>
-				F I N A L P R O J E C T
-			</Typography>
 
 		</div>
 	)
