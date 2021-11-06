e89a1255c4e8e2bdd41f544efcb3203ad30cb8b7
Daniil <79855065+Daniil-Khlyvniuk@users.noreply.github.com>
2021-11-07 01:18:52 +0200

    Update Category.js

--- client/src/components/Category/Category.js
+++ client/src/components/Category/Category.js
@@ -5,8 +5,6 @@ import { Child, Parent } from './categoryElems'
 
 const Category = () => {
 	const categoryTree = UseCategoryTree()
-	// eslint-disable-next-line no-console
-	// console.log('categoryTree', categoryTree)
 	const categoryList = UseMakeNodeChild(categoryTree, Parent, Child)
 	return (
 		<Parent root>
