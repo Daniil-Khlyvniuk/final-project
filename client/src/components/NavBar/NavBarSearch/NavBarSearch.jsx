99c7b89f27dede4d40a5fdfb479c5c09c77bdbf4
Vitaliy Gukalov <gukalov.v@ukr.net>
2021-11-07 02:36:38 +0200

    add alternate search component

--- client/src/components/NavBar/NavBarSearch/NavBarSearch.jsx
+++ client/src/components/NavBar/NavBarSearch/NavBarSearch.jsx
@@ -1,52 +1,99 @@
-import { Box } from '@mui/system'
-import React, { useState } from 'react'
+// import { Box } from '@mui/system'
+// import React, { useState } from 'react'
+// import SearchIcon from './SearchIcon/SearchIcon'
+// import { Search, StyledAlert, StyledInputBase } from './styles'
+// import { useDispatch, useSelector } from 'react-redux'
+// // import { setAllProducts } from '../../../store/Products/productsSlice'
+// import {productsActions, productsOperations, productsSelectors } from '../../../store/Products'
+
+// const HeaderSearch = () => {
+// 	const getProductsList = useSelector(productsSelectors.getProducts())
+// 	const [search, setSearch] = useState('')
+// 	const dispatch = useDispatch()
+// 	let timer
+
+// 	const handleChange = (e) => {
+// 		clearTimeout(timer)
+
+// 		timer = setTimeout(() => {
+// 			if (!e.target.value) {
+// 				dispatch(productsOperations.fetchProducts())
+// 				setSearch('')
+// 				return
+// 			}
+
+// 			dispatch(productsActions.setAllProducts(
+// 				getProductsList.filter((good) =>
+// 					good.name.toLowerCase().includes(e.target.value.toLowerCase())
+// 				)
+// 			))
+// 		}, 1000)
+// 		setSearch(e.target.value)
+// 	}
+
+// 	const warning = <StyledAlert severity="info">Product not found</StyledAlert>
+
+// 	return (
+// 		<Box>
+// 			<Search>
+// 				<SearchIcon />
+// 				<StyledInputBase
+// 					placeholder="Search..."
+// 					type='search'
+// 					value={search}
+// 					onChange={handleChange}
+// 					sx={{ borderBottom: '1px solid #373F41' }}
+// 				/>
+// 			</Search>
+// 			{!getProductsList.length && search && warning}
+// 		</Box>
+// 	)
+// }
+
+// export default HeaderSearch
+
+import * as React from 'react'
+import TextField from '@mui/material/TextField'
 import SearchIcon from './SearchIcon/SearchIcon'
-import { Search, StyledAlert, StyledInputBase } from './styles'
-import { useDispatch, useSelector } from 'react-redux'
-// import { setAllProducts } from '../../../store/Products/productsSlice'
-import {productsActions, productsOperations, productsSelectors } from '../../../store/Products'
+import { StyledAutocomplete, StyledBox } from './styles'
+// import { productsActions } from '../../../store/Products'
+
+const products = [
+	{
+		label: 'Cotton Dark Blue Bed Linen',
+		price: '$280',
+		img: 'img/card1.png'
+	},
+	{
+		label: 'Phistachio French Linen',
+		price: '$220',
+		img: 'img/card2.png'
+	},
+	{
+		label: 'Light Pink Bed Linen',
+		price: '$270',
+		img: 'img/card3.png'
+	},
+	{
+		label: 'French Dark Green Linen',
+		price: '$250',
+		img: 'img/card4.png'
+	}
+]
 
 const HeaderSearch = () => {
-	const getProductsList = useSelector(productsSelectors.getProducts())
-	const [search, setSearch] = useState('')
-	const dispatch = useDispatch()
-	let timer
-
-	const handleChange = (e) => {
-		clearTimeout(timer)
-
-		timer = setTimeout(() => {
-			if (!e.target.value) {
-				dispatch(productsOperations.fetchProducts())
-				setSearch('')
-				return
-			}
-
-			dispatch(productsActions.setAllProducts(
-				getProductsList.filter((good) =>
-					good.name.toLowerCase().includes(e.target.value.toLowerCase())
-				)
-			))
-		}, 1000)
-		setSearch(e.target.value)
-	}
-
-	const warning = <StyledAlert severity="info">Product not found</StyledAlert>
-
 	return (
-		<Box>
-			<Search>
-				<SearchIcon />
-				<StyledInputBase
-					placeholder="Search..."
-					type='search'
-					value={search}
-					onChange={handleChange}
-					sx={{ borderBottom: '1px solid #373F41' }}
-				/>
-			</Search>
-			{!getProductsList.length && search && warning}
-		</Box>
+		<StyledBox>
+			<StyledAutocomplete
+				disablePortal
+				id="combo-box-demo"
+				options={products}
+				// options={productsActions.setAllProducts()}
+				size='small'
+				renderInput={(params) => <TextField {...params} label="Search..." />}
+			/>
+			<SearchIcon />
+		</StyledBox>
 	)
 }
 
--- client/src/components/NavBar/NavBarSearch/styles.js
+++ client/src/components/NavBar/NavBarSearch/styles.js
@@ -1,5 +1,7 @@
 import { styled } from '@mui/material/styles'
 import { Alert, InputBase } from '@mui/material'
+import { Box } from '@mui/system'
+import Autocomplete from '@mui/material/Autocomplete'
 
 export const Search = styled('div')(() => ({
 	display: 'flex',
@@ -25,5 +27,20 @@ export const StyledAlert = styled(Alert)(() => ({
 	backgroundColor: 'unset',
 	'& .css-acap47-MuiAlert-message, & .MuiAlert-icon': {
 		padding: '5px 0',
+	}
+}))
+
+export const StyledBox = styled(Box)(() => ({
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+}))
+
+export const StyledAutocomplete = styled(Autocomplete)(() => ({
+	width: 380,
+	'& .css-1i7vg6k-MuiFormLabel-root-MuiInputLabel-root': {
+		fontWeight: 600,
+		fontSize: '16px',
+		color: '#373F41',
 	}
 }))
