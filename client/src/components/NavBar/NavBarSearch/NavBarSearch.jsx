56cde6b36f8da86e81c8801e33b90211d73a15aa
Vitaliy Gukalov <gukalov.v@ukr.net>
2021-11-09 03:09:24 +0200

    added livesearch logic (80%), styles and search optimization

--- client/src/components/NavBar/NavBarSearch/NavBarSearch.jsx
+++ client/src/components/NavBar/NavBarSearch/NavBarSearch.jsx
@@ -1,79 +1,97 @@
 import React, { useState } from 'react'
 import TextField from '@mui/material/TextField'
 import SearchIcon from './SearchIcon/SearchIcon'
-import { StyledAutocomplete, StyledBox } from './styles'
+import { Search, SearchIconWrapper, StyledAutocomplete, StyledBox } from './styles'
 import productsAPI from '../../../utils/API/productsAPI'
 import { Grid, Avatar, Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
 
 const HeaderSearch = () => {
 	const [options, setOptions] = useState([])
+	let timer
 
-	const onSearch = async (e) => {
-		if (e.target.value.trim().length === 0) {
-			setOptions([])
-			return
-		}
+	const onSearch = (e) => {
+		clearTimeout(timer)
 
-		try {
-			const res = await productsAPI.searchForProducts({ query: e.target.value })
-			const data = await res.data
+		timer = setTimeout(async function () {
 
-			if (data.length > 0) {
-				setOptions(data)
-			} else {
+			if (e.target.value.trim().length === 0) {
+				setOptions([])
+				return
+			}
+
+			try {
+				const res = await productsAPI.searchForProducts(
+					{ query: e.target.value }
+				)
+				const data = await res.data
+
+				if (data.length > 0) {
+					setOptions(data)
+				} else {
+					setOptions([])
+				}
+			} catch (err) {
+				// eslint-disable-next-line no-console
+				console.error(err)
 				setOptions([])
 			}
-		} catch (err) {
-			// eslint-disable-next-line no-console
-			console.error(err)
-			setOptions([])
-		}
+		}, 500)
 	}
 
 	return (
 		<StyledBox>
-			<StyledAutocomplete
-				id="combo-box-demo"
-				options={options}
-				onKeyUp={onSearch}
-				size='small'
-				// noOptionsText='product not found'
-				// loading={true}
-				renderInput={(params) => <TextField {...params} label="Search..." variant="standard" />}
-				getOptionLabel={(option) => option.name}
-				renderOption={(props, option) => {
-					return (
-						<Link to={`/product-details/${option._id}`} {...props} style={{ textDecoration: 'none' }}>
-							<Grid container wrap="nowrap" spacing={2}>
-								<Grid item>
-									<Avatar
-										src={option.imageUrls[0]}
-										alt={option.name}
-										sx={{ width: 50, height: 50 }}
-										variant='square'
-									/>
+			<Search>
+				<SearchIconWrapper>
+					<SearchIcon />
+				</SearchIconWrapper>
+				<StyledAutocomplete
+					id="combo-box-demo"
+					options={options}
+					onKeyUp={onSearch}
+					size='small'
+					renderInput={(params) => <TextField {...params} label="Search..." variant="standard" />}
+					getOptionLabel={(option) => `${option.name} 
+						${option.currentPrice} 
+						${option.itemNo} 
+						${option.categories}
+					`}
+					renderOption={(props, option) => {
+						return (
+							<Link to={`/product-details/${option._id}`}
+								style={{ textDecoration: 'none' }}
+								{...props}
+							>
+								<Grid container wrap="nowrap" spacing={2}>
+									<Grid item>
+										<Avatar
+											src={option.imageUrls[0]}
+											alt={option.name}
+											sx={{ width: 50, height: 50 }}
+											variant='square'
+										/>
+									</Grid>
+									<Grid item xs zeroMinWidth>
+										<Typography
+											variant='menuBold'
+											sx={{ display: 'block' }}
+										>
+											{option.name} ({option.itemNo})
+										</Typography>
+										<Typography
+											variant='footerTextMedium'
+											sx={{ display: 'block' }}
+										>
+											{'$' + option.currentPrice}
+										</Typography>
+									</Grid>
 								</Grid>
-								<Grid item xs zeroMinWidth>
-									<Typography
-										variant='menuBold'
-										sx={{ display: 'block' }}
-									>
-										{option.name}
-									</Typography>
-									<Typography
-										variant='footerTextMedium'
-										sx={{ display: 'block' }}
-									>
-										{'$' + option.currentPrice}
-									</Typography>
-								</Grid>
-							</Grid>
-						</Link>
-					)
-				}}
-			/>
-			<SearchIcon />
+							</Link>
+						)
+					}}
+				>
+				</StyledAutocomplete>
+			</Search>
 		</StyledBox>
 	)
 }
--- client/src/components/NavBar/NavBarSearch/styles.js
+++ client/src/components/NavBar/NavBarSearch/styles.js
@@ -10,9 +10,28 @@ export const StyledBox = styled(Box)(() => ({
 
 export const StyledAutocomplete = styled(Autocomplete)(() => ({
 	width: 380,
+	position: 'relative',
 	'& .css-y2vtlc-MuiFormLabel-root-MuiInputLabel-root': {
 		fontWeight: 600,
 		fontSize: '16px',
 		color: '#373F41',
+		left: 30
+	},
+	'& .MuiAutocomplete-inputRoot': {
+		paddingLeft: 30
 	}
+}))
+
+export const Search = styled('div')(() => ({
+	position: 'relative',
+}))
+
+export const SearchIconWrapper = styled('div')(() => ({
+	height: '100%',
+	position: 'absolute',
+	top: 5,
+	pointerEvents: 'none',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
 }))
