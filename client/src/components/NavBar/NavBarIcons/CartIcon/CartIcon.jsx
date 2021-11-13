16ae6bcba2c285fe9d54e7eae8b9d0f6dbffd768
Vitaliy Gukalov <64445967+Vitaliy-1809@users.noreply.github.com>
2021-11-13 07:54:11 +0200

    universal modal component created, login modal opens by click on man … (#66)
    * universal modal component created, login modal opens by click on man icon in header
    
    * finished universal modal component and its calling from App.js, styles due to the layout, click on cart button

--- client/src/components/NavBar/NavBarIcons/CartIcon/CartIcon.jsx
+++ client/src/components/NavBar/NavBarIcons/CartIcon/CartIcon.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
 import Badge from '@mui/material/Badge'
 import IconButton from '@mui/material/IconButton'
 import LocalMallIcon from '@mui/icons-material/LocalMall'
 import { useStyles } from './styles'
+import { useDispatch } from 'react-redux'
+import modalActions from '../../../../store/Modal'
+import CartModal from '../../../Modal/CartModal'
 
 const Carticon = () => {
 	const classes = useStyles()
+	const dispatch = useDispatch()
+	const handleOpen = (content) => dispatch(modalActions.handleOpen(content))
 
 	return (
-		<IconButton aria-label="cart" sx={{ padding: 0 }}>
-			<Badge badgeContent={4} color="success">
-				<NavLink exact to='/cart' className={classes.navbarLink}>
-					<LocalMallIcon />
-				</NavLink>
+		<IconButton aria-label="cart" sx={{ padding: 0 }} onClick={() => handleOpen(<CartModal />)}>
+			<Badge badgeContent={4} color="success" className={classes.navbarLink}>
+				<LocalMallIcon />
 			</Badge>
 		</IconButton>
 	)
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,13 +2,22 @@ import { configureStore } from '@reduxjs/toolkit'
 import productsSlice from './Products/productsSlice'
 import linksSlice from './Links/linksSlice'
 import categorySlice from './Category/categorySlice'
+import modalSlice from './Modal/modalSlice'
 
 const store = configureStore({
 	reducer: {
 		products: productsSlice,
 		links: linksSlice,
-		category: categorySlice
-	}
+		category: categorySlice,
+		modal: modalSlice,
+	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				// Ignore these action types
+				ignoredActions: ['modal/handleOpen'],
+			},
+		}),
 })
 
 export default store
