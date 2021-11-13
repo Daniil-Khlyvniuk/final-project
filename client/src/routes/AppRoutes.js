6d2f3cc95775ece8f88f7a2c86d60629807d01ac
Vitaliy Gukalov <gukalov.v@ukr.net>
2021-11-13 18:06:34 +0200

    deleted useless pages and routes fromappRoutes

--- client/src/routes/AppRoutes.js
+++ client/src/routes/AppRoutes.js
@@ -7,29 +7,22 @@ import ProductDetails from '../pages/ProductDetails/ProductDetails'
 import Catalog from '../pages/Catalog/Catalog'
 import PaymentAndDelivery from '../staticPages/PaymentAndDelivery/PaymentAndDelivery'
 import Returns from '../staticPages/Returns/Returns'
-// import About from '../staticPages/About/About'
 import PrivacyPolicy from '../staticPages/PrivacyPolicy/PrivacyPolicy'
+import TermsOfService from '../staticPages/TermsOfService/TermsOfService'
 import AboutUs from '../staticPages/AboutUs/AboutUs'
-import Reviews from '../staticPages/Reviews/Reviews'
-// import Blog from '../staticPages/Blog/Blog'
-import Login from '../pages/Login/Login'
 import Cart from '../pages/Cart/Cart'
-import Contact from '../staticPages/Contact/Contact'
 import Page404 from '../pages/Page404/Page404'
 import Favorites from '../pages/Favorites/Favorites'
 import HandleSubscribe from '../pages/HandleSubscribe'
-
-
-// get isLoggedIn from Redux
+// import { useSelector } from 'react-redux'
 
 const AppRoutes = () => {
+	// const user = useSelector(state => state.user.data)
+	// const isLoggedIn = !!user
 
 	return (
 		<Switch>
 			<Route exact path='/'><Main /></Route>
-			{/* <Route exact path='/about'><About /></Route>  */}
-			<Route exact path='/about/contact-us'><Contact /></Route>
-			<Route exact path='/login'><Login /></Route>
 			<ProtectedRoute exact path='/favorites' isLoggedIn={false}>
 				<Favorites />
 			</ProtectedRoute>
@@ -40,13 +33,9 @@ const AppRoutes = () => {
 			<Route exact path='/shop/payment-and-delivery'><PaymentAndDelivery /></Route>
 			<Route exact path='/shop/returns'><Returns /></Route>
 			<Route exact path='/shop/privacy-policy'><PrivacyPolicy /></Route>
-			<Route exact path='/shop/terms-of-service'>terms-of-service</Route>
+			<Route exact path='/shop/terms-of-service'><TermsOfService /></Route>
 			<Route exact path='/about/about-us'><AboutUs /></Route>
-			<Route exact path='/about/careers'><Reviews /></Route>
-			{/* <Route exact path='/blog'><Blog /></Route> */}
-
 			<Route exact path='/subscription/:email'><HandleSubscribe /></Route>
-
 			<Route exact path='*'><Page404 /></Route>
 		</Switch>
 	)
