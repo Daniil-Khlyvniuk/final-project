6824a5e91619d32566c6fd74f3b7d76fd00d9d49
Vitaliy Gukalov <64445967+Vitaliy-1809@users.noreply.github.com>
2021-11-13 11:22:23 +0200

    Update index.js

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -10,7 +10,7 @@ const store = configureStore({
 		products: productsSlice,
 		links: linksSlice,
 		category: categorySlice,
-    modal: modalSlice,
+    		modal: modalSlice,
 		shoppingBag: shoppingBagSlice,
 	},
 	middleware: (getDefaultMiddleware) =>
