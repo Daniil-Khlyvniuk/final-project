9c0aa318da205ec1a040100ae699bdd29f99d3ff
Elizabeth <77597107+lizarudayaa@users.noreply.github.com>
2021-11-14 11:20:47 +0200

    Single product page & product description component  (#79)
    * started work on popular component
    
    * added header and card
    
    * Add json file and popular list and card components
    
    * updated styles and moved them to the theme. Started work on redux-toolkit slice for popular
    
    * added header styles to cardlist and popular component to main page
    
    * connected server to popular
    
    * hardcode version
    
    * added active product reducer
    
    * added styles to product details page and product dexription component
    
    * changed material ui components and added new styling
    
    * updated reducer
    
    * backup
    
    * try to start project
    
    * try to start project
    
    * merge develop
    
    * merge develop
    
    * backup
    
    * backup
    
    * finished component
    
    * fixed btn styles
    
    * merge
    
    * layout for product page
    
    Co-authored-by: Elizabeth Rudaya <elizabethrudaya@Elizabeths-MacBook-Pro.local>

--- client/src/utils/API/productsAPI.js
+++ client/src/utils/API/productsAPI.js
@@ -4,6 +4,17 @@ const getAllProducts = (options) => axios.get(`/api/products${options ? `?${opti
 
 const getOneProduct = (productId) => axios.get(`/api/products/${productId}`)
 
+
+// eslint-disable-next-line max-len
+const getColors = (productId) => axios.get(`/api/products/info/color/${productId}`)
+// eslint-disable-next-line max-len
+const getSizes = (productId) => axios.get(`/api/products/info/size/${productId}`)
+// eslint-disable-next-line max-len
+const getNewVariant =(specification, specificationId , productId)=> axios.get(`/api/products/variant/${specification}/${specificationId}/${productId}`)
+
+// eslint-disable-next-line max-len
+const getProductVariant = (productId , variantId) => axios.get(`/api/products/${productId}/${variantId}`)
+
 const addNewProduct = (product) => axios.post('/api/products', product)
 // product is an object with all the product's properties like name, quantity, brand etc.
 
@@ -23,10 +34,10 @@ const searchForProducts = (searchPhrases) => axios.post('/api/products/search', 
 //                                      					}
 
 const searchAutocomplete = (searchPhrases) => axios.post('/api/products/autocomplete', searchPhrases)
-// SearchPhrases has to be an object with a "query" key and searchwords separated by spaces: 
+// SearchPhrases has to be an object with a "query" key and searchwords separated by spaces:
 // 																								{
 //                                      						query: 'linen square beige'
-//         
+//
 
 // eslint-disable-next-line max-len
 const getFilteredProducts = (filterParams) => axios.get(`/api/products/filter${filterParams}`)
@@ -35,9 +46,15 @@ const getFilteredProducts = (filterParams) => axios.get(`/api/products/filter${f
 export default {
 	getAllProducts,
 	getOneProduct,
+	getProductVariant,
 	addNewProduct,
 	updateProduct,
 	searchForProducts,
+	getFilteredProducts,
+	getColors,
+	getSizes,
+	getNewVariant,
 	searchAutocomplete,
-	getFilteredProducts
+
+
 }
