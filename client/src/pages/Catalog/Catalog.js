2a4a2b91dd1c50aefe8943b39f7db27114276ee1
Bobshop verbytski <nikolasdelunko@gmail.com>
2021-11-16 21:15:19 +0200

    Done button and cards

--- client/src/pages/Catalog/Catalog.js
+++ client/src/pages/Catalog/Catalog.js
@@ -1,37 +1,103 @@
-import { Container } from '@mui/material'
-import ProductsCatalog from '../../components/Catalog/Catalog'
 import * as React from 'react'
-import { styled } from '@mui/material/styles'
 import Box from '@mui/material/Box'
-import Paper from '@mui/material/Paper'
 import Grid from '@mui/material/Grid'
+import ProductsCatalog from '../../components/Catalog/Catalog'
+import Typography from '@mui/material/Typography'
+import theme from '../../utils/Theme'
+import { ThemeProvider } from '@mui/material/styles'
+import Button from '@mui/material/Button'
+import { Link as RouterLink } from 'react-router-dom'
 
-const Item = styled(Paper)(({ theme }) => ({
-	...theme.typography.body2,
-	padding: theme.spacing(4),
-	textAlign: 'center',
-	color: theme.palette.text.secondary,
-}))
+const textStyle = {
+	color: '#373F41',
+	fontSize: '32px',
+	display: 'flex',
+	justifyContent: 'center',
+	margin: '50px auto',
+}
+
+const headSearch = {
+	display: 'flex',
+	justifyContent: 'space-between',
+	margin: '50px auto',
+	maxWidth: '880px',
+}
+
 
 export default function Catalog() {
 	return (
-		<Container maxWidth="lg">
-			<Box sx={{ flexGrow: 3 }} columns={2}>
-				<Grid container spacing={3}>
-					<Grid item xs={3}>
-						<Item>xs=8</Item>
+		<div>
+			<ThemeProvider theme={theme}>
+				<p></p>
+				<Box sx={{ flexGrow: 1 }}>
+					<Grid container spacing={2}>
+						<Grid item xs={3}>
+							<Typography
+								style={textStyle}
+								variant={'h2'}>
+								CATALOG
+							</Typography>
+						</Grid>
+						<Grid item xs={9}>
+							<Grid style={headSearch} item xs={12}>
+								<Button
+									to='/'
+									component={RouterLink}
+									color="primary"
+									variant="outlined"
+								>SHOP ALL
+								</Button>
+								<Button
+									to='/'
+									component={RouterLink}
+									color="primary"
+									variant="outlined"
+								>BEDROOM
+								</Button>
+								<Button
+									to='/'
+									component={RouterLink}
+									color="primary"
+									variant="outlined"
+								>BED LINEN
+								</Button>
+								<Button
+									to='/'
+									component={RouterLink}
+									color="primary"
+									variant="outlined"
+								>KITCHEN
+								</Button>
+								<Button
+									to='/'
+									component={RouterLink}
+									color="primary"
+									variant="outlined"
+								>BATHROOM
+								</Button>
+								<Button
+									to='/'
+									component={RouterLink}
+									color="primary"
+									variant="outlined"
+								>LOUNGEWEAR
+								</Button>
+								<Button
+									to='/'
+									component={RouterLink}
+									color="primary"
+									variant="outlined"
+								>SALE
+								</Button>
+							</Grid>
+							<Grid item xs={12}>
+								<ProductsCatalog />
+							</Grid>
+						</Grid>
 					</Grid>
-					<Grid item xs={9}>
-						<Item>xs=4</Item>
-					</Grid>
-					<Grid item xs={9}>
-						<Item>
-							<ProductsCatalog />
-						</Item>
-					</Grid>
-				</Grid>
-			</Box>
-		</Container>
+				</Box>
+			</ThemeProvider>
+		</div>
 	)
 }
 
