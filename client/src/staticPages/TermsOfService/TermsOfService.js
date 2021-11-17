fd71f8285039642fa6d85fda2fcbb62cc9d17972
Elizabeth Rudaya <lizarudayaa@gmail.com>
2021-11-17 19:48:53 +0200

    fixed slider on product details page

--- client/src/staticPages/TermsOfService/TermsOfService.js
+++ client/src/staticPages/TermsOfService/TermsOfService.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 const TermsOfService = () => {
+	const [data, setData] = useState(null)
+	const [loading , setLoading]= useState(false)
+
+	useEffect(() => {
+		setLoading(true)
+		axios('../termsofservice.json')
+			.then(r => {
+				setData(r.data)
+				setLoading(false)
+			})
+	}, [])
 	return (
+
 		<h1>
 			TermsOfService
+			{loading && <p>Loading</p>}
+			{data && <p>Data found!</p>}
 		</h1>
 	)
 }
