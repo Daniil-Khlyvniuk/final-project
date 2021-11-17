fc6db3ea55d95a5ed100beec37a64193046b75af
Elizabeth Rudaya <lizarudayaa@gmail.com>
2021-11-17 20:38:36 +0200

    started work on TermsOfService page

--- client/src/staticPages/TermsOfService/TermsOfService.js
+++ client/src/staticPages/TermsOfService/TermsOfService.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import axios from 'axios'
+import {Container, Grid} from '@mui/material'
 
 const TermsOfService = () => {
 	const [data, setData] = useState(null)
@@ -18,7 +19,35 @@ const TermsOfService = () => {
 		<h1>
 			TermsOfService
 			{loading && <p>Loading</p>}
-			{data && <p>Data found!</p>}
+			{data && <Container maxWidth='lg' sx={{mt:'80px'}}>
+				<Grid container>
+					<Grid item md={2}>
+						{
+
+							data.map(item => {
+								return <div key={item.pageTitle}>
+									<h6>{item.pageTitle}</h6>
+									<ul>
+										{item.tabs.map(tab => {
+											return <li key={tab.tabTitle}>{tab.tabTitle}</li>
+										})}
+									</ul>
+								</div>
+							
+							}
+							)
+						}
+					</Grid>
+					<Grid item md={10}>
+						{!loading && data.map((data) => {
+							data.tabs.map((item, index) => {
+								return <h1 key={index}>Hello!!!</h1>
+							})
+						})}
+					</Grid>
+				</Grid>
+
+			</Container>}
 		</h1>
 	)
 }
