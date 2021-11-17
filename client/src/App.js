79a222b57fc3af2339f5eb298268b736d161167d
Elizabeth <77597107+lizarudayaa@users.noreply.github.com>
2021-11-17 21:11:46 +0200

    Product description (#88)
    * started work on popular component
    
    * added header and card
    
    * Add json file and popular list and card components
    
    * updated styles and moved them to the theme. Started work on redux-toolkit slice for popular
    
    * added header styles to cardlist and popular component to main page
    
    * connected server to popular
    
    * hardcode version
    
    * added active product reducer
    
    * added styles to product details page and product dexription component
    
    * changed material ui components and added new styling
    
    * updated reducer
    
    * backup
    
    * try to start project
    
    * try to start project
    
    * merge develop
    
    * merge develop
    
    * backup
    
    * backup
    
    * finished component
    
    * fixed btn styles
    
    * merge
    
    * layout for product page
    
    * fixed filter
    
    * fixed filter by color for product page
    
    * fixed filter
    
    * fixed console errors
    
    * fixed allVarians
    
    * last version
    
    * added actions to buttons
    
    * moved variant feching to redux
    
    * added termsofservice.json
    
    * fixed slider on product details page
    
    * Update package.json
    
    backup
    
    * started work on TermsOfService page
    
    * backup
    
    * Delete .DS_Store
    
    Co-authored-by: Elizabeth Rudaya <elizabethrudaya@Elizabeths-MacBook-Pro.local>

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, {useEffect} from 'react'
+
 import Header from './components/NavBar/NavBar'
 import Footer from './components/Footer'
 import AppRoutes from './routes/AppRoutes'
--- client/src/pages/ProductDetails/ProductDetails.js
+++ client/src/pages/ProductDetails/ProductDetails.js
@@ -1,62 +1,64 @@
-import React, {useEffect} from 'react'
+import React, {useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import {Container, Grid, Alert, Backdrop, CircularProgress} from '@mui/material'
 import {useDispatch, useSelector} from 'react-redux'
-
-import {activeProductOperations, activeProductSelector} from '../../store/ActiveProduct'
+import  {ProductOperations, ProductSelector} from '../../store/Product'
+import {Alert, Backdrop, CircularProgress, Container, Grid} from '@mui/material'
 import ProductDescription from '../../components/ProductDescription/ProductDescription'
 import Carousel from '../../components/Carousel/Carousel'
-import RelatedItemsList from '../../components/RelatedItems/RelatedItemsList'
 
 const ProductDetails = () => {
 
 	const { id } = useParams()
 	const dispatch = useDispatch()
 
+	const isLoading = useSelector(ProductSelector.isLoading())
+	const activeProduct = useSelector(ProductSelector.getProduct())
+	const parent = useSelector(ProductSelector.getParent())
+
+
 	useEffect(() => {
-		dispatch(activeProductOperations.fetchActiveProduct(id))
+		dispatch(ProductOperations.fetchProductUrl(id))
 	}, [id, dispatch])
 
-	const isLoading = useSelector(activeProductSelector.isLoading())
-	const activeProduct = useSelector(activeProductSelector.getActiveVariant())
+	useEffect(() => {
+		if(activeProduct){
+			dispatch(ProductOperations.fetchAllColors(parent._id))
+			dispatch(ProductOperations.fetchSizes({
+				colorId: activeProduct.color._id,
+				productId: activeProduct.product._id}))
+			dispatch(ProductOperations.fetchAllVariants(parent.variants))
 
-
-	const parent = useSelector(activeProductSelector.getParent())
-
-	useEffect(()=>{
-		if(activeProduct && parent){
-			dispatch(activeProductOperations.fetchSizes(parent._id))
-			dispatch(activeProductOperations.fetchColors(parent._id))
 		}
-	// eslint-disable-next-line react-hooks/exhaustive-deps
-	},[activeProduct])
+	}, [activeProduct, dispatch])
 
 
 
 	if (!activeProduct ) {
 		return <Alert severity='error'>Product not found</Alert>
 	}
-
+	
 	return (
-		<Container maxWidth="lg">
+		<Container maxWidth='lg' sx={{mt:'80px'}}>
 			{isLoading && (
 				<Backdrop
-					sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+					sx={{ color: 'white', zIndex: (theme) => theme.zIndex.drawer + 1 }}
 					open={isLoading}>
 					<CircularProgress color="inherit" />
 				</Backdrop>)}
-			{activeProduct && (
-				<Grid sx={{mt:'80px'}} container spacing={2} >
-					<Grid item md={6} xs={12}><Carousel  /></Grid>
-					<Grid item md={6} xs={12}>
-						{activeProduct && <ProductDescription />}
-					</Grid>
-					<Grid sx={{mt:'80px'}} item md={12}>
-						<RelatedItemsList />
-					</Grid>
-				</Grid>)}
+			{activeProduct && <Grid container spacing={2}>
+				<Grid item md={6} xs={12} >
+					<Carousel slides={activeProduct.imageUrls} product={true} />
+				</Grid>
+				<Grid item md={6} xs={12}>
+					<ProductDescription />
+				</Grid>
+				<Grid sx={{mt:'80px'}} item md={12}>
+					Recently viewed
+				</Grid>
+			</Grid>}
 		</Container>
 	)
-}
 
+}
+	
 export default ProductDetails
