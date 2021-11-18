3723309235cae96624e0040f56dbd67d7a7830ea
shootka <pirathaker315@gmail.com>
2021-11-18 18:38:40 +0200

    infinite scroll - done!

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, {useEffect} from 'react'
 
-import Header from './components/NavBar/NavBar'
-import Footer from './components/Footer'
-import AppRoutes from './routes/AppRoutes'
+// import Header from './components/NavBar/NavBar'
+// import Footer from './components/Footer'
+// import AppRoutes from './routes/AppRoutes'
 import { useSelector } from 'react-redux'
 import { modalSelectors } from './store/Modal'
 import useAuthToken from './utils/customHooks/useAuthToken'
-import ScrollButton from './components/ScrollButton/ScrollButton'
+// import ScrollButton from './components/ScrollButton/ScrollButton'
+import Scroll from './components/Scroll/Scroll'
 
 
 
@@ -20,10 +21,11 @@ const App = () => {
 	
 	return (
 		<div className={'App'}>
-			<Header />
-			<AppRoutes />
-			<Footer />
-			<ScrollButton/>
+			{/*<Header />*/}
+			{/*<AppRoutes />*/}
+			{/*<Footer />*/}
+			{/*<ScrollButton/>*/}
+			<Scroll/>
 			{modal}
 		</div>
 	)
