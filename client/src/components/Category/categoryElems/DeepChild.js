a0a3614205ed5886dd5ff0d7cdce5ef13e68d08c
Vitaliy Gukalov <gukalov.v@ukr.net>
2021-11-20 14:19:48 +0200

    add routes to categories

--- client/src/components/Category/categoryElems/DeepChild.js
+++ client/src/components/Category/categoryElems/DeepChild.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { StyledMenuItem } from './style'
 import PropTypes from 'prop-types'
+import { useHistory } from 'react-router'
 
 const DeepChild = ({ text }) => {
+	let history = useHistory()
+	// ...?category=${text}
 	return (
-		<StyledMenuItem>
+		// eslint-disable-next-line max-len
+		<StyledMenuItem onClick={() => history.push(`/api/products/filter?category=${text}`)}>
 			{ text }
 		</StyledMenuItem>
 	)
