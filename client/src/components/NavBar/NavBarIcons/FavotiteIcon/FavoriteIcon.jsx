069f461d20ef83405d3d414b0bc78c81abba0f29
Vitaliy Gukalov <gukalov.v@ukr.net>
2021-11-26 00:10:01 +0200

    add favorites counter logic in header icon and adding favorites products login logic

--- client/src/components/NavBar/NavBarIcons/FavotiteIcon/FavoriteIcon.jsx
+++ client/src/components/NavBar/NavBarIcons/FavotiteIcon/FavoriteIcon.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
-import { IconButton } from '@mui/material'
+import { Badge, IconButton } from '@mui/material'
 import { useStyles } from './styles'
+import { useSelector } from 'react-redux'
+import { favoritesSelectors } from '../../../../store/Favorites'
 
 const Favoriteicon = () => {
 	const classes = useStyles()
+	const favorites = useSelector(favoritesSelectors.getFavorites())
 
 	return (
-		<IconButton aria-label="favorites" title='Favorites' sx={{ padding: 0 }}>
-			<NavLink exact to='/favorites' className={classes.navbarLink}>
-				<FavoriteBorderIcon />
-			</NavLink>
+		<IconButton
+			aria-label="favorites" title='Favorites'
+			sx={{ padding: 0 }}
+		>
+			<Badge badgeContent={favorites?.length} color="success">
+				<NavLink exact to='/favorites' className={classes.navbarLink}>
+					<FavoriteBorderIcon />
+				</NavLink>
+			</Badge>
 		</IconButton>
 	)
 }
--- client/src/components/ProductCard/FavotiteIcon/FavoriteIcon.jsx
+++ client/src/components/ProductCard/FavotiteIcon/FavoriteIcon.jsx
@@ -2,29 +2,25 @@ import React from 'react'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
 import FavoriteIcon from '@mui/icons-material/Favorite'
 import { IconButton } from '@mui/material'
-// eslint-disable-next-line no-unused-vars
 import { useSelector, useDispatch } from 'react-redux'
-// eslint-disable-next-line no-unused-vars
 import { favoritesOperations, favoritesSelectors } from '../../../store/Favorites'
 import PropTypes from 'prop-types'
-// import modalActions from '../../../store/Modal'
-// import LoginModal from '../../Modal/LoginModal'
-// import { userSelectors } from '../../../store/User'
+import modalActions from '../../../store/Modal'
+import LoginModal from '../../Modal/LoginModal'
+import { userSelectors } from '../../../store/User'
 
 const Favoriteicon = ({ id }) => {
-	// const user = useSelector(userSelectors.getToken())
+	const user = useSelector(userSelectors.getToken())
 	const favorites = useSelector(favoritesSelectors.getFavorites())
 	// eslint-disable-next-line no-console
 	console.log('favorites', favorites)
 
 	const dispatch = useDispatch()
-	// const handleOpen = (content) => dispatch(modalActions.modalToggle(content))
-	let favoritesStorage = JSON.parse(localStorage.getItem('favorites')) || []
+	const handleOpen = (content) => dispatch(modalActions.modalToggle(content))
+	const favoritesStorage = JSON.parse(localStorage.getItem('favorites')) || []
 
 	const addToFavorites = () => {
 		if (!localStorage.getItem('favorites')) localStorage.setItem('favorites', JSON.stringify([]))
-
-		let favoritesStorage = JSON.parse(localStorage.getItem('favorites'))
 
 		if (favoritesStorage.includes(id)) {
 			const index = favoritesStorage.indexOf(id)
@@ -39,7 +35,7 @@ const Favoriteicon = ({ id }) => {
 	return (
 		<IconButton
 			aria-label="favorites"
-			title={favoritesStorage.includes(id) ? 'remove from favorites' : 'add to favorites'} // check id
+			title={favoritesStorage.includes(id) ? 'remove from favorites' : 'add to favorites'}
 			sx={{
 				padding: 0,
 				position: 'absolute',
@@ -47,9 +43,10 @@ const Favoriteicon = ({ id }) => {
 				top: '10px',
 				zIndex: 333
 			}}
-			// eslint-disable-next-line no-console
-			// onClick={!user ? () => handleOpen(<LoginModal />) : console.log('test')}
-			onClick={() => addToFavorites()}
+			onClick={!user
+				? () => handleOpen(<LoginModal />)
+				: addToFavorites
+			}
 		>
 			{favoritesStorage.includes(id)
 				? <FavoriteIcon fontSize="large" />
@@ -64,3 +61,74 @@ Favoriteicon.propTypes = {
 }
 
 export default Favoriteicon
+
+
+
+// import React, { useState } from 'react'
+// import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
+// import FavoriteIcon from '@mui/icons-material/Favorite'
+// import { IconButton } from '@mui/material'
+// import { useSelector, useDispatch } from 'react-redux'
+// // eslint-disable-next-line no-unused-vars
+// import { favoritesOperations, favoritesSelectors } from '../../../store/Favorites'
+// import PropTypes from 'prop-types'
+// import modalActions from '../../../store/Modal'
+// import LoginModal from '../../Modal/LoginModal'
+// import { userSelectors } from '../../../store/User'
+
+// const Favoriteicon = ({ id }) => {
+// 	const [, setFav] = useState([])
+// 	const user = useSelector(userSelectors.getToken())
+// 	// const favorites = useSelector(favoritesSelectors.getFavorites())
+// 	const dispatch = useDispatch()
+// 	const handleOpen = (content) => dispatch(modalActions.modalToggle(content))
+// 	let favoritesStorage = JSON.parse(localStorage.getItem('favorites')) || []
+
+// 	// useEffect(() => {
+// 	// }, [favoritesStorage])
+
+// 	const addToFavorites = () => {
+// 		if (!localStorage.getItem('favorites')) localStorage.setItem('favorites', JSON.stringify([]))
+
+// 		if (favoritesStorage.includes(id)) {
+// 			const index = favoritesStorage.indexOf(id)
+// 			favoritesStorage.splice(index, 1)
+// 		} else {
+// 			favoritesStorage.push(id)
+// 		}
+// 		favoritesOperations.fetchFavorites(favoritesStorage)(dispatch)
+// 		localStorage.setItem('favorites', JSON.stringify(favoritesStorage))
+
+// 		const tesy = JSON.parse(localStorage.getItem('favorites'))
+// 		setFav(tesy)
+// 	}
+
+// 	return (
+// 		<IconButton
+// 			aria-label="favorites"
+// 			title={favoritesStorage.includes(id) ? 'remove from favorites' : 'add to favorites'} // check id
+// 			sx={{
+// 				padding: 0,
+// 				position: 'absolute',
+// 				right: '10px',
+// 				top: '10px',
+// 				zIndex: 333
+// 			}}
+// 			onClick={!user
+// 				? () => handleOpen(<LoginModal />)
+// 				: addToFavorites
+// 			}
+// 		>
+// 			{favoritesStorage.includes(id)
+// 				? <FavoriteIcon fontSize="large" />
+// 				: <FavoriteBorderIcon fontSize="large" />
+// 			}
+// 		</IconButton>
+// 	)
+// }
+
+// Favoriteicon.propTypes = {
+// 	id: PropTypes.string
+// }
+
+// export default Favoriteicon
