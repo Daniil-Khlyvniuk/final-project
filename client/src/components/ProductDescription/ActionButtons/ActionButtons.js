82a3b762a42551569e9b61f7cce5603f2be937c9
Vitaliy Gukalov <gukalov.v@ukr.net>
2021-11-27 13:17:36 +0200

    favorites finished

--- client/src/components/ProductDescription/ActionButtons/ActionButtons.js
+++ client/src/components/ProductDescription/ActionButtons/ActionButtons.js
@@ -2,39 +2,41 @@ import React from 'react'
 import { Button } from '@mui/material'
 import shoppingBagReducer from '../../../store/ShoppingBag'
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined'
-// import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined'
+import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined'
 import { useDispatch, useSelector } from 'react-redux'
 import { ProductSelector } from '../../../store/Product'
 import { userSelectors } from '../../../store/User'
-// import modalActions from '../../../store/Modal'
-// import { favoritesOperations } from '../../../store/Favorites'
-// import LoginModal from '../../Modal/LoginModal'
+import modalActions from '../../../store/Modal'
+import { favoritesOperations, favoritesSelectors } from '../../../store/Favorites'
+import LoginModal from '../../Modal/LoginModal'
 
 const ActionButtons = () => {
 	const dispatch = useDispatch()
 	const activeProduct = useSelector(ProductSelector.getProduct())
 	const parent = useSelector(ProductSelector.getParent())
 	const user = useSelector(userSelectors.getToken())
-	// const handleOpen = (content) => dispatch(modalActions.modalToggle(content))
-	// const favoritesStorage = JSON.parse(localStorage.getItem('favorites')) || []
+	// eslint-disable-next-line no-unused-vars
+	const favorites = useSelector(favoritesSelectors.getFavorites())
+	const handleOpen = (content) => dispatch(modalActions.modalToggle(content))
+	const favoritesStorage = JSON.parse(localStorage.getItem('favorites')) || []
 
-	// const addToFavorites = () => {
-	// 	if (!localStorage.getItem('favorites')) localStorage.setItem('favorites', JSON.stringify([]))
+	const addToFavorites = () => {
+		if (!localStorage.getItem('favorites')) localStorage.setItem('favorites', JSON.stringify([]))
 
-	// 	if (favoritesStorage.includes(id)) {
-	// 		const index = favoritesStorage.indexOf(id)
-	// 		favoritesStorage.splice(index, 1)
-	// 	} else {
-	// 		favoritesStorage.push(id)
-	// 	}
-	// 	favoritesOperations.fetchFavorites(favoritesStorage)(dispatch)
-	// 	localStorage.setItem('favorites', JSON.stringify(favoritesStorage))
-	// }
+		if (favoritesStorage.includes(activeProduct._id)) {
+			const index = favoritesStorage.indexOf(activeProduct._id)
+			favoritesStorage.splice(index, 1)
+		} else {
+			favoritesStorage.push(activeProduct._id)
+		}
+		favoritesOperations.fetchFavorites(favoritesStorage)(dispatch)
+		localStorage.setItem('favorites', JSON.stringify(favoritesStorage))
+	}
 
 	return (
 		<>
 			<Button disableRipple
-				sx={{ mx: '13px', padding: { lg: '21px 33px', md: '16px', sm: '10px' } }}
+				sx={{ mx: '13px', padding: { lg: '21px 33px', md: '14px', sm: '10px' } }}
 				variant={'contained'}
 				onClick={() => {
 					// eslint-disable-next-line max-len
@@ -52,18 +54,17 @@ const ActionButtons = () => {
 				ADD TO BAG
 			</Button>
 			<Button disableRipple
-				disabled={!user}
+				title={favoritesStorage.includes(activeProduct._id) ? 'remove from favorites' : 'add to favorites'}
 				sx={{ padding: { lg: '22px', md: '16px', sm: '12px', xs: '9px' } }} variant={'contained'}
-				// onClick={!user
-				// 	? () => handleOpen(<LoginModal />)
-				// 	: addToFavorites
-				// }
+				onClick={!user
+					? () => handleOpen(<LoginModal />)
+					: addToFavorites
+				}
 			>
-				{/* {favoritesStorage.includes(id)
+				{favoritesStorage.includes(activeProduct._id)
 					? <FavoriteOutlinedIcon fontSize={'small'} />
 					: <FavoriteBorderOutlinedIcon fontSize={'small'} />
-				} */}
-				<FavoriteBorderOutlinedIcon fontSize={'small'} />
+				}
 			</Button>
 		</>
 	)
