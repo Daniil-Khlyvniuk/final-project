9940b415669f2592873b55d496d021eff73201ac
alexmaybee <aleksandr.mayboroda@gmail.com>
2021-11-28 23:06:11 +0300

    fixed filters exept scroll

--- client/src/store/Products/operations.js
+++ client/src/store/Products/operations.js
@@ -33,7 +33,31 @@ const fetchProductsByFilter = createAsyncThunk(
 	}
 )
 
+const fetchProductsByNextPage = createAsyncThunk(
+	'products/fetchProductsByNextPage',
+	async (_, {getState,rejectWithValue}) => {
+		try
+		{
+			const filters = getState().filter.data
+			const newQueryString = 
+				queryString.stringify(filters,{
+					arrayFormat: 'comma',
+					skipNull: true,
+					skipEmptyString: true,
+					parseNumbers: true
+				})
+			const res = await productsApi.getFilteredProducts(newQueryString)
+			return res.data
+		}
+		catch(error)
+		{
+			return rejectWithValue(error.message)
+		}
+	}
+)
+
 export default {
 	fetchProducts,
 	fetchProductsByFilter,
+	fetchProductsByNextPage,
 }
