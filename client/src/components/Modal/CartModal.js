67578860f6b0370a7fda02168bfca12d87800551
Vitaliy Gukalov <gukalov.v@ukr.net>
2021-12-01 18:52:59 +0200

    CartModal fixed

--- client/src/components/Modal/CartModal.js
+++ client/src/components/Modal/CartModal.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import BasicModal from './Modal'
-import PropTypes from 'prop-types'
 import useHandleShoppingBag from '../../utils/customHooks/useHandleShoppingBag'
 import CardInModal from '../CardInModal/CardInModal'
 import { Button, Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
+import { Box } from '@mui/system'
+import { useDispatch } from 'react-redux'
+import modalActions from '../../store/Modal'
 
 const style = {
 	modal: {
@@ -32,44 +34,61 @@ const style = {
 	}
 }
 
-const CartModal = ({ handleClose, openModal }) => {
+const CartModal = () => {
 	const { shoppingBag, totalPrice } = useHandleShoppingBag()
+	const dispatch = useDispatch()
+	const handleClose = () => dispatch(modalActions.modalToggle(false))
 
 	return (
 		<BasicModal
 			style={style.modal}
-			body={<>
-				{shoppingBag?.length ? <><Typography fontSize={18}
-					style={style.title}>TOTAL: USD
-          ${totalPrice}</Typography>
-				<Link exact to={'/cart'} style={style.link}>
-					<Button onClick={handleClose} variant={'contained'} style={style.button}>CHECKOUT</Button>
-				</Link>
-				<div style={{ overflowY: 'scroll' }}>{shoppingBag?.map((item, key) => <CardInModal
-					price={item?.currentPrice}
-					image={'/' + item?.imageUrls[0]}
-					title={item?.product?.name}
-					key={key}
-					size={item?.size?.name}
-					color={item?.color?.name}
-					amount={item?.amount}
-				/>)}</div>
-				</> : <div style={{ margin: '3rem auto' }}>
-					<Typography fontSize={32} variant={'h2'} style={{ textAlign: 'center', padding: '0 2rem' }}>Your bag is
-            feeling lonely - add some beautiful new to it!</Typography>
-					<Button variant={'contained'} style={{ margin: '2rem auto 0', display: 'block' }} onClick={handleClose}>CONTINUE
-            SHOPPING</Button>
-				</div>}
-			</>}
-			handleClose={handleClose}
-			openModal={openModal}
+			body={
+				<Box>
+					{shoppingBag?.length
+						? <Box>
+							<Typography
+								fontSize={18}
+								style={style.title}>
+								TOTAL: USD${totalPrice}
+							</Typography>
+							<Link exact to={'/cart'} style={style.link}>
+								<Button
+									onClick={handleClose}
+									variant={'contained'}
+									style={style.button}>
+									CHECKOUT
+								</Button>
+							</Link>
+							<Box style={{ overflowY: 'scroll' }}>
+								{shoppingBag?.map((item, key) =>
+									(<CardInModal
+										price={item?.currentPrice}
+										image={'/' + item?.imageUrls[0]}
+										title={item?.product?.name}
+										key={key}
+										size={item?.size?.name}
+										color={item?.color?.name}
+										amount={item?.amount}
+									/>))}
+							</Box>
+						</Box>
+						: <Box style={{ margin: '3rem auto' }}>
+							<Typography
+								fontSize={32}
+								variant={'h2'}
+								style={{ textAlign: 'center', padding: '0 2rem' }}>
+								Your bag is feeling lonely - add some beautiful new to it!
+							</Typography>
+							<Button
+								variant={'contained'}
+								style={{ margin: '2rem auto 0', display: 'block' }}
+								onClick={handleClose}>
+								CONTINUE SHOPPING
+							</Button>
+						</Box>}
+				</Box>}
 		/>
 	)
 }
 
 export default CartModal
-
-CartModal.propTypes = {
-	handleClose: PropTypes.func,
-	openModal: PropTypes.bool,
-}
