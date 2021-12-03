24aea1c047e13a2d3d309b42ed7b861e6d338662
Daniil Khlyvniuk <daniil.khlyv@gmail.com>
2021-12-03 19:09:00 +0200

    fixed login

--- server/controllers/customers.js
+++ server/controllers/customers.js
@@ -90,12 +90,12 @@ exports.loginCustomer = async (req, res, next) => {
     return res.status(400).json(errors);
   }
 
-	console.log(req.body)
-	const { subscribe, userData } = req.body
+	// console.log(req.body)
+	// const { subscribe, userData } = req.body
 
 
-	const loginOrEmail = userData.loginOrEmail;
-  const password = userData.password;
+	const loginOrEmail = req.body.loginOrEmail;
+  const password = req.body.password;
   const configs = await getConfigs();
 
   // Find customer by email
@@ -133,9 +133,9 @@ exports.loginCustomer = async (req, res, next) => {
             }
           );
 
-					if (subscribe) [
-
-					]
+					// if (subscribe) [
+					//
+					// ]
 
         } else {
           errors.password = "Password incorrect";
