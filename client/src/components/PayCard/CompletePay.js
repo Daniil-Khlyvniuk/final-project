17698598c0feff5dae85acb836b3cdbba98a413e
Yaroslav Verbytski <nikolasdelunko@gmail.com>
2021-12-07 20:19:09 +0200

    Checkout (#181)
    * add form
    
    * Beefore merge
    
    * done 50%
    
    * nawerstal tyt pochti vse
    
    * need add little logik
    
    * zbz VSe
    
    * DAL DAL
    
    * summ 85% done
    
    * almost done
    
    * need adaptive
    
    * after merge
    
    * Almost Done
    
    * link in related
    
    * fix Name
    
    * repiar api cart
    
    * fix this shit
    
    * Payment done
    
    * Cart done
    
    * CArt Good
    
    * after merge
    
    * Cart repiar
    
    * beefore merge
    
    * after merge
    
    * cart form check done
    
    * tyda syda popravil i vso ok
    
    * done write order
    
    * array add

--- client/src/components/PayCard/CompletePay.js
+++ client/src/components/PayCard/CompletePay.js
@@ -1,20 +1,43 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Box, Button, Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
 import useHandleShoppingBag from '../../utils/customHooks/useHandleShoppingBag'
 import cartAPI from '../../utils/API/cartAPI'
+import axios from 'axios'
+// import { useSelector } from 'react-redux'
+// import { userSelectors } from '../../store/User'
 
 const CompletePay = () => {
 	const newOrder = cartAPI.getCart()
-
 	const OrderNum = Date.now()
 	const handleShoppingBag = useHandleShoppingBag()
+	const [userData, setUserData] = useState({})
+	const [BuyGoods, setBuyGoods] = useState({})
+	const shoppingBag = JSON.parse(localStorage.getItem('shoppingBag') || '[]') || []
+	// const user = useSelector(userSelectors.getData())
+	// const isLoggedIn = !!user
+
+
+	useEffect(() => {
+		axios('/api/customers/customer')
+			.then(res =>setUserData(res.data))
+		setBuyGoods(shoppingBag)
+		handleShoppingBag.AfterBuy()
+	}, [])
 
 	useEffect(()=> {
-		console.log(newOrder)
 		cartAPI.addOrder(newOrder)
-		handleShoppingBag.AfterBuy()
 	},[])
+
+	const newShoppingBag = [{
+		UserInfo: {...userData},
+		OrderNumber: OrderNum,
+		ShoppingBag: BuyGoods
+
+	}]
+	localStorage.setItem('ORDER2', JSON.stringify(newShoppingBag))
+
+
 
 	return (
 		<Box style={{textAlign: 'center', margin: '7rem 0'}}>
