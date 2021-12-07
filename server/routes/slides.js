0f1dfa48fc6c772efc04db46f4f37c5d58a96cd1
shootka <pirathaker315@gmail.com>
2021-12-07 18:03:28 +0200

    add some fixes

--- server/routes/slides.js
+++ server/routes/slides.js
@@ -15,7 +15,7 @@ const {
 // @access  Private
 router.post(
   "/",
-  // passport.authenticate("jwt-admin", { session: false }),
+  passport.authenticate("jwt-admin", { session: false }),
   addSlide
 );
 
@@ -24,7 +24,7 @@ router.post(
 // @access  Private
 router.put(
   "/:customId",
-  // passport.authenticate("jwt-admin", { session: false }),
+  passport.authenticate("jwt-admin", { session: false }),
   updateSlide
 );
 
