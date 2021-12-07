f1784a1d431cae28eca4bebbb815c18d6139c89e
alexmaybee <aleksandr.mayboroda@gmail.com>
2021-12-07 13:41:28 +0300

    oops 2

--- client/src/utils/customHooks/useHelmet.jsx
+++ client/src/utils/customHooks/useHelmet.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 const UseHelmet = () => {
 
-  const returnCeo = ({lang, title, description, keywords}) => {
-    return (
-      <>
-        <html lang={lang} />
-        <title>{title}</title>
-        <meta name="description" content={description} />
-        <meta name="keywords" content={keywords} />
-      <>
-    )
-  }
+	const returnCeo = ({lang, title, description, keywords}) => {
+		return (
+			<>
+				<html lang={lang} />
+				<title>{title}</title>
+				<meta name="description" content={description} />
+				<meta name="keywords" content={keywords} />
+			</>
+		)
+	}
 
-  return {returnCeo}
+	return {returnCeo}
 }
 
 export default UseHelmet
