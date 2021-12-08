b0ffdd6b232ae856103f9cdd5936214845dda747
Elizabeth Rudaya <lizarudayaa@gmail.com>
2021-12-08 15:44:13 +0200

    fixed useEffect issue with double re-render

--- client/src/pages/ProductDetails/ProductDetails.js
+++ client/src/pages/ProductDetails/ProductDetails.js
@@ -1,4 +1,4 @@
-import React, { useEffect , useState } from 'react'
+import React, { useEffect  } from 'react'
 import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import productActions, { ProductOperations, ProductSelector } from '../../store/Product'
@@ -10,11 +10,12 @@ import BackdropLoader from '../../components/UI/BackdropLoader/BackdropLoader'
 import UseCeo from '../../utils/customHooks/useCeo'
 import { favoritesOperations } from '../../store/Favorites'
 
+
 const ProductDetails = () => {
 	const { id } = useParams()
 	const dispatch = useDispatch()
-	const [isLoading , setIsLoading] = useState(false)
-	// const isLoading = useSelector(ProductSelector.isLoading())
+
+	const isLoading = useSelector(ProductSelector.isLoading())
 	const activeProduct = useSelector(ProductSelector.getProduct())
 	const parent = useSelector(ProductSelector.getParent())
 	const favorites = JSON.parse(localStorage.getItem('favorites')) || []
@@ -28,43 +29,36 @@ const ProductDetails = () => {
 	}))
 	
 	useEffect(() => {
-		// eslint-disable-next-line no-console
-		console.log('LOADDDINNNNGGG FAV')
 		favoritesOperations.fetchFavorites(favorites)(dispatch)
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [favorites.length])
 
 	useEffect(() => {
-		// eslint-disable-next-line no-console
-		console.log('LOADDDINNNNGGG ID')
 		dispatch(ProductOperations.fetchProductUrl(id))
+			.then((res) => {
+				dispatch(ProductOperations.fetchAllColors(res.payload._id))
+				dispatch(ProductOperations.fetchSizes({
+					colorId: res.payload.variants.color,
+					productId: res.payload._id
+				}))
+				dispatch(ProductOperations.fetchAllVariants(res.payload._id))
+
+			})
 
 		return function cleanup(){
 			dispatch(productActions.clearActiveProduct())
 		}
 	}, [id, dispatch])
 
-	useEffect(() => {
-		setIsLoading(true)
-		// eslint-disable-next-line no-console
-		console.log('LOADDDINNNNGGG PARENT')
-		if (activeProduct) {
-			dispatch(ProductOperations.fetchAllColors(parent._id))
-			dispatch(ProductOperations.fetchSizes({
-				colorId: activeProduct.color,
-				productId: parent._id
-			}))
-			dispatch(ProductOperations.fetchAllVariants(parent._id))
 
-		}
-		setIsLoading(false)
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [activeProduct])
-
-	// if (isLoading) {
-	//
-	// 	return <BackdropLoader open={isLoading}/>
-	// }
+	if(isLoading){
+		return (
+			<Container
+				sx={{width:'100%', height:'1200px'}}>
+				<BackdropLoader open={isLoading}/>
+			</Container>
+		)
+	}
 
 	if(!activeProduct){
 		return <StyledTypography>Product not found</StyledTypography>
@@ -83,8 +77,6 @@ const ProductDetails = () => {
 				}
 			/>
 			<Container maxWidth="lg" sx={{ mt: '80px' }}>
-
-				{isLoading &&  <BackdropLoader open={isLoading}/>}
 				{activeProduct && <><Grid container spacing={4}>
 					<Grid item md={6} xs={12}>
 						<Carousel slides={activeProduct.imageUrls} product={true}/>
