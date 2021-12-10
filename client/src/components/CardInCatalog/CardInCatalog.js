c2a7d694cc077f0c1b2dbf5e47efef8512de7f70
Daniil Khlyvniuk <daniil.khlyv@gmail.com>
2021-12-10 22:06:39 +0200

    added variants to produc card

--- client/src/components/CardInCatalog/CardInCatalog.js
+++ client/src/components/CardInCatalog/CardInCatalog.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import OtherVariants from './otherVariants/otherVariants'
 import { useStyles } from './styles'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
@@ -15,7 +16,8 @@ import { IconButton } from '@mui/material'
 import LoginModal from '../Modal/LoginModal/LoginModal'
 import { grey } from '@mui/material/colors'
 
-const CardInCatalog = ({ title, image, price, _id }) => {
+
+const CardInCatalog = ({ title, image, price, _id, parentId, color }) => {
 	const classes = useStyles()
 	const user = useSelector(userSelectors.getData())
 	// eslint-disable-next-line no-unused-vars
@@ -27,7 +29,7 @@ const CardInCatalog = ({ title, image, price, _id }) => {
 	useEffect(() => {
 		favoritesOperations.fetchFavorites(favoritesStorage)(dispatch)
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [favoritesStorage.length])
+	}, [ favoritesStorage.length ])
 
 	const addToFavorites = () => {
 		if (!localStorage.getItem('favorites')) localStorage.setItem('favorites', JSON.stringify([]))
@@ -43,92 +45,110 @@ const CardInCatalog = ({ title, image, price, _id }) => {
 	}
 
 	return (
-		<Box className={classes.container}>
+		<Box className={ classes.container }>
 			<img
-				className={classes.img}
-				src={image} alt='img'
-				data-testid='image-catalog-card'
+				className={ classes.img }
+				src={ image } alt="img"
+				data-testid="image-catalog-card"
 			/>
 			<p
-				className={classes.cardTitle}
-				data-testid='title-catalog-card-main'
+				className={ classes.cardTitle }
+				data-testid="title-catalog-card-main"
 			>
-				{stringSlice(title, 22)}
+				{ stringSlice(title, 22) }
 				<span
-					className={classes.cardPrice}
-					data-testid='price-catalog-card-main'
+					className={ classes.cardPrice }
+					data-testid="price-catalog-card-main"
 				>
-					{price} $
+					{ price } $
 				</span>
 			</p>
 			<Tooltip title="Click for details">
+
+
 				<Box
-					className={classes.blockHover}
-					sx={{ fontSize: { sm: '10px' } }}
+					className={ classes.blockHover }
+					sx={ { fontSize: { sm: '10px' } } }
 				>
 					<IconButton
 						aria-label="favorites"
-						title={favoritesStorage.includes(_id) ? 'remove from favorites' : 'add to favorites'}
-						sx={{
+						title={ favoritesStorage.includes(_id) ? 'remove from favorites' : 'add to favorites' }
+						sx={ {
 							padding: 0,
 							position: 'absolute',
 							right: '10px',
-							top: '10px',
-						}}
-						onClick={!user
+							top: '10px'
+						} }
+						onClick={ !user
 							? async () => {
 								await handleOpen(<LoginModal />)
 								await !favoritesStorage.includes(_id) && addToFavorites()
 							}
 							: addToFavorites
 						}
-						data-testid='favorite-button-catalog-card'
+						data-testid="favorite-button-catalog-card"
 					>
-						{favoritesStorage.includes(_id) && user
+						{ favoritesStorage.includes(_id) && user
 							? <FavoriteIcon
-								sx={{
+								sx={ {
 									color: grey[50],
-									fontSize: { xl: '35px', lg: '35px', md: '30px', sm: '30px', xs: '40px' }
-								}}
+									fontSize: {
+										xl: '35px',
+										lg: '35px',
+										md: '30px',
+										sm: '30px',
+										xs: '40px'
+									}
+								} }
 							/>
 							: <FavoriteBorderIcon
-								sx={{
+								sx={ {
 									color: grey[50],
-									fontSize: { xl: '35px', lg: '35px', md: '30px', sm: '30px', xs: '40px' }
-								}}
+									fontSize: {
+										xl: '35px',
+										lg: '35px',
+										md: '30px',
+										sm: '30px',
+										xs: '40px'
+									}
+								} }
 							/>
 						}
 					</IconButton>
 					<Link
-						to={`/product-details/${_id}`}
-						style={{ textDecoration: 'none' }}
+						to={ `/product-details/${ _id }` }
+						style={ { textDecoration: 'none' } }
 					>
-						<Box className={classes.contentWrapper}>
+						<Box className={ classes.contentWrapper }>
 							<p
-								className={classes.hoverTitle}
-								data-testid='title-catalog-card-hover'
+								className={ classes.hoverTitle }
+								data-testid="title-catalog-card-hover"
 							>
-								{stringSlice(title, 25)}
+								{ stringSlice(title, 25) }
 							</p>
 							<p
-								className={classes.hoverPrice}
-								data-testid='price-catalog-card-hover'
+								className={ classes.hoverPrice }
+								data-testid="price-catalog-card-hover"
 							>
-								{price} $
+								{ price } $
 							</p>
+
+							<OtherVariants currColor={ color } parentId={ parentId } />
 						</Box>
 					</Link>
 				</Box>
 			</Tooltip>
-		</Box >
+		</Box>
 	)
 }
 
 CardInCatalog.propTypes = {
 	_id: PropTypes.string,
+	parentId: PropTypes.string,
 	title: PropTypes.string,
 	image: PropTypes.string,
 	price: PropTypes.number,
+	color: PropTypes.object
 }
 
 export default CardInCatalog
