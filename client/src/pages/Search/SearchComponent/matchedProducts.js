07894d720993d4cc6a5d684627ba2854991c9e31
Daniil Khlyvniuk <daniil.khlyv@gmail.com>
2021-12-12 19:49:01 +0200

    started refactoring code

--- client/src/pages/Search/SearchComponent/matchedProducts.js
+++ client/src/pages/Search/SearchComponent/matchedProducts.js
@@ -17,12 +17,17 @@ const MatchedProducts = () => {
 				variant="h2"
 				sx={ { fontSize: { md: 32, xs: 25 } } }
 			>
+				{/* eslint-disable-next-line no-undef */ }
 				{ `Search results for "${ search_term }"` }
 			</Typography>
+			{/* eslint-disable-next-line no-undef */ }
 			{ perPageArray.length && (
 				<DropDownSelect
+					/* eslint-disable-next-line no-undef */
 					arrayToIterate={ perPageArray }
+					/* eslint-disable-next-line no-undef */
 					selectedValue={ perPage }
+					/* eslint-disable-next-line no-undef */
 					selectHandler={ setPerPage }
 					label={ 'Show' }
 				/>
