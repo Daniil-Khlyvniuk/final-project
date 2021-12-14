63c710beb4eb534fde2a78d6a0f5675f29510414
Vitaliy Gukalov <gukalov.v@ukr.net>
2021-12-14 20:55:45 +0200

    AppRoutes fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import Header from './components/NavBar/NavBar'
 import Footer from './components/Footer'
-import AppRoutes from './routes/appRoutes'
+import AppRoutes from './routes/AppRoutes'
 import { useSelector, useDispatch } from 'react-redux'
 import { modalSelectors } from './store/modal'
 import useAuth from './utils/customHooks/useAuth'
