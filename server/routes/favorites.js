fbfcbc46d5fef07b5fb0e4a54faf5d8f0707c54d
Daniil <79855065+Daniil-Khlyvniuk@users.noreply.github.com>
2021-12-24 20:51:58 +0200

    Favorites prod  server (#235)
    * fixed product variant quantity after buying
    
    * fixed product variant quantity after buying
    
    * fixed orders
    
    * replace favorites to server

---
+++ server/routes/favorites.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+
+const {
+  getFavorites,
+  updateFavorites,
+  toggleProductInFavorites,
+  getFavoritesIds,
+} = require("../controllers/favorites");
+
+router.post(
+  "/:productId",
+  passport.authenticate("jwt", { session: false }),
+  toggleProductInFavorites
+);
+
+router.put(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  updateFavorites
+);
+
+router.get("/", passport.authenticate("jwt", { session: false }), getFavorites);
+router.get(
+  "/ids",
+  passport.authenticate("jwt", { session: false }),
+  getFavoritesIds
+);
+
+module.exports = router;
