e296044f4824b85b4351b49417c155b6af369512
Vitaliy Gukalov <gukalov.v@ukr.net>
2022-01-08 11:50:13 +0200

    added background-color in email texts

--- client/src/utils/emailTemplates/index.js
+++ client/src/utils/emailTemplates/index.js
@@ -46,19 +46,19 @@ export const subscribeTemlate = (email) => `<!DOCTYPE html>
                         <div style="padding:25px 0;font-weight:300;">
                           <h2 style="margin-block-start: 1em;
                           margin-block-end: 1em;text-align: center;
-                          background-color: rgb(255 255 255 / 49%);
+                          background-color: rgba(255,255,255,.5);
                           padding-top: 10px;padding-bottom: 10px;
                           margin-bottom: 35px;">Thanks for join us!</h1>
                           <h3 style="margin-block-start: 1em;
                           margin-block-end: 1em; text-align: center;
-                          background-color: rgb(255 255 255 / 49%);
-                          margin-bottom: 30px;"
+                          margin-bottom: 30px;
+                          background-color: rgba(255,255,255,.5);"
                           >You have subscribed to our news</h2>
                           <p style="margin-block-start: 1em;
                           margin-block-end: 1em;
-                          background-color: rgb(255 255 255 / 49%);
                           padding-top: 10px;padding-bottom: 10px; 
-                          text-align: center;"
+                          text-align: center;
+                          background-color: rgba(255,255,255,.5);"
                           >Fresh and exclusive offers are now coming your way, 
                           plus a heads up on our
                            <strong>latest arrivals</strong> 
@@ -66,7 +66,7 @@ export const subscribeTemlate = (email) => `<!DOCTYPE html>
                            so you'll always have the <strong>best deal</strong>.
                            </p>
                   
-                          <p style="background-color: rgb(255 255 255 / 49%); 
+                          <p style="background-color: rgba(255,255,255,.5); 
                           padding-top: 10px; padding-bottom: 10px; 
                           text-align: center;">We're giving you 
                           <strong>15% off</strong> your first order. 
@@ -75,7 +75,7 @@ export const subscribeTemlate = (email) => `<!DOCTYPE html>
                           
                         </div>
                         <div style="text-align: center; margin-bottom: 20px;">
-                          <p style="background-color: rgb(255 255 255 / 49%);
+                          <p style="background-color: rgba(255,255,255,.5);
                           padding-top: 10px; padding-bottom: 10px;
                           text-align: center; margin-bottom: 30px;">
                             If you want to unsubscribe go to  
@@ -149,12 +149,13 @@ export const unSubscribeTemlate = (email) => `<!DOCTYPE html>
                           <h3 style="margin-block-start: 1em;
                           margin-block-end: 1em; text-align: center;
                           background-color: rgb(255 255 255 / 49%);
-                          margin-bottom: 30px;"
+                          margin-bottom: 30px;
+                          background-color: rgba(255,255,255,.5);"
                           >You have unsubscribed from our news</h2>
                           
                         </div>
                         <div style="text-align: center; margin-bottom: 20px;">
-                          <p style="background-color: rgb(255 255 255 / 49%);
+                          <p style="background-color: rgba(255,255,255,.5);
                           padding-top: 10px; padding-bottom: 10px;
                           text-align: center; margin-bottom: 30px;">
                             If you want to resubscribe go to  
