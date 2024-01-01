2f1375c0718884c00a8756f7008ae239629d8b88
Bobshop verbytski <nikolasdelunko@gmail.com>
2024-01-01 20:27:15 +0100

    sasd

--- server/server.js
+++ server/server.js
@@ -73,6 +73,7 @@ app.use("/", mainRoute);
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
+	
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
